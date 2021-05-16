8271ef30d8b5acab68ed86a5026570278dc085e0
Christopher Hosken <hoskenchristopher@gmail.com>
2021-05-16 20:30:37 +0700

    initial commit

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,16 @@
 import discord
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
+from dotenv.main import dotenv_values
 
 client = commands.Bot(command_prefix=".")
-token = os.getenv("DISCORD_BOT_TOKEN")
+config = dotenv_values(".env")
+token = config['CYCLES_X_BOT_TOKEN']
 
 @client.event
 async def on_ready() :
-    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Currently in development."))
     print("I am online")
 
 @client.command()
@@ -21,6 +24,5 @@ async def whoami(ctx) :
 @client.command()
 async def clear(ctx, amount=3) :
     await ctx.channel.purge(limit=amount)
-
 
 client.run(token)
